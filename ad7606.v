1bf5a6f79db12a7e68cb574c338d2706bca18b4d
markthumes <markthumes@gmail.com>
2023-07-10 13:00:25 -0500

    Added state machine and data clocking

--- ad7606.v
+++ ad7606.v
@@ -14,11 +14,16 @@ module delay #(
 	parameter TICKS = 50
 )(
 	input wire clk,
+	input wire rst,
 	input wire start,
 	output reg done
 );
 	initial done = 0;
 	reg [$clog2(TICKS):0] ctr = 0;
+	always @(rst) begin 
+		done <= 0;
+		ctr  <= 0;
+	end
 	always @(posedge clk) begin
 		if( start && !done ) begin
 			if( ctr >= TICKS ) begin
@@ -30,32 +35,16 @@ module delay #(
 	end
 endmodule
 
-module delay2 #(
-	parameter TIME_NS = 50,
-	parameter TIME_PER_TICK_NS = 33
-)(
-	input wire clk,
-	input wire start,
-	output reg done
-);
-	initial done = 0;
-	localparam T_TICKS = $ceil(50/TIME_PER_TICK_NS);
-	reg [$clog2(T_TICKS):0] ctr = 0;
-	always @(posedge clk) begin
-		if( start && !done ) begin
-			if( ctr >= T_TICKS ) begin
-				ctr <= 0;
-				done <= 1;
-			end
-			else ctr <= ctr + 1;
-		end
-	end
-endmodule
 module ad7606#(
 	parameter CLK_FREQUENCY = 30_000_000
 )(
 	input wire clk,   //max clock 
 	input wire power,  //Control signal for power up/down of device
+	input wire busy,
+	output reg conv,
+	output wire n_cs,
+	output wire sclk,
+	output wire reset,
 	output reg stby
 );
 	initial stby = 0;
@@ -78,6 +67,15 @@ module ad7606#(
 	localparam POWER_ON_TIME_TICKS = POWER_ON_TIME_MS * TICKS_PER_MS;
 `endif
 
+	//_________________________________________________________//
+	//                  DEFINE STATE MACHINE                   //
+	localparam STATE_POWER = 2'd0;
+	localparam STATE_RESET = 2'd1;
+	localparam STATE_CONV  = 2'd2;
+	localparam STATE_READ  = 2'd3;
+	reg [1:0] state = STATE_POWER;
+	//---------------------------------------------------------//
+
 	wire power_rise;
 	wire power_fall;
 	sync powersync(clk,power,power_rise,power_fall);
@@ -88,23 +86,91 @@ module ad7606#(
 			begin_power <= 1;
 			stby <= 1;
 		end
-		else if( power_fall ) stby <= 0;
+		else if( power_fall ) begin
+			begin_power <= 0;
+			stby <= 0;
+		end
 	end
+	
+	wire rst;
+	assign rst = begin_power;
 
 	wire powerup_complete;
 	delay #(POWER_ON_TIME_TICKS)
-	powerdelay(clk, begin_power, powerup_complete);
+	powerdelay(clk, rst, begin_power, powerup_complete);
 
 	//handle reset
 	wire reset_done;
 	delay #($ceil(50/TIME_PER_TICK_NS))
-	resetdelay(clk, powerup_complete, reset_done);
+	resetdelay(clk, rst, powerup_complete, reset_done);
 
 	//handle delay before we can start program
 	wire conv_ready;
 	delay #($ceil(25/TIME_PER_TICK_NS))
-	convdelay(clk, reset_done, conv_ready);
+	convdelay(clk, rst, reset_done, conv_ready);
 
+	assign reset = powerup_complete && !reset_done;
 
+	//_________________________________________________________//
+	//                   HANDLE CONVERSION                     //
+	always @(posedge clk) begin
+		if( state == STATE_CONV && cycle_ctr == 0 )begin
+			conv <= 1;
+		end
+		else conv <= 0;
+	end
+	//---------------------------------------------------------//
+
+	//_________________________________________________________//
+	//   Sync busy to the FPGA using a 3 bit shift register    //
+	reg [2:0] BUSYr;
+	always @(posedge clk) BUSYr <= {BUSYr[1:0], busy};
+	//Now we can detect falling edges for busy bit
+	wire BUSY_falling = (BUSYr[2:1]==2'b10);
+	wire BUSY_rising  = (BUSYr[2:1]==2'b01);
+	//---------------------------------------------------------//
+
+	//_________________________________________________________//
+	//                     CHANGE STATES                       //
+	always @(rst) begin
+		state <= STATE_POWER;
+	end
+	always @(posedge clk) begin
+		if( state == STATE_POWER ) begin
+			if( powerup_complete ) state <= STATE_RESET;
+		end
+		if( state == STATE_RESET ) begin
+			if( conv_ready ) state <= STATE_CONV;
+		end
+		if( BUSY_falling ) state <= STATE_READ;
+		if( cycle_ctr >= 4 ) state <= STATE_CONV;
+	end
+	//---------------------------------------------------------//
+
+	//_________________________________________________________//
+	//                    CS AND CLOCK
+	//This does not guarantee timing is met per data sheet     //
+	localparam CYCLES = 4;
+	localparam COUNTS = 17; //16 bits and a chip select
+	reg [$clog2(CYCLES):0] cycle_ctr = 0;
+	reg [$clog2(COUNTS):0] data_ctr = 0;
+	always @(posedge clk) begin
+		if( state == STATE_READ )begin
+			if( data_ctr < COUNTS ) begin
+				data_ctr  <= data_ctr + 1;
+			end
+			else begin
+				data_ctr  <= 0;
+				cycle_ctr <= cycle_ctr + 1;
+			end
+		end
+		else begin
+			cycle_ctr <= 0;
+			data_ctr  <= 0;
+		end
+	end
+	
+	assign n_cs = (clk && (data_ctr >= 1) && (data_ctr <   2)) || (state == STATE_CONV);
+	assign sclk = (clk && (data_ctr >= 2) && (data_ctr <  18));
 
 endmodule
--- testbench.v
+++ testbench.v
@@ -3,18 +3,40 @@
 module top_tb();
 	reg clk = 0;
 	reg pwr = 0;
+	reg busy = 0;
 	
 	wire ADC_STBY;
+	wire ADC_CONV;
+	wire ADC_nCS;
+	wire ADC_SCLK;
+	wire ADC_RESET;
 
 	initial begin
 		#100
 		pwr = 1;
+		#3000
+		pwr = 0;
+		#100
+		pwr = 1;
+	end
+
+	always @(posedge ADC_CONV) begin
+		#20
+		busy = 1;
+		#100
+		busy = 0;
 	end
 
 	ad7606 adc(
 		.clk(clk),
 		.power(pwr),
-		.stby(ADC_STBY) );
+		.busy(busy),
+		.conv(ADC_CONV),
+		.n_cs(ADC_nCS),
+		.sclk(ADC_SCLK),
+		.reset(ADC_RESET),
+		.stby(ADC_STBY)
+	 );
 
 	//sim time: 10000 * 1 ns = 10 us;
 	localparam DURATION = 40000; //total sim time
